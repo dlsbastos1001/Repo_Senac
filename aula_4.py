25b328bfdeb0cbaccce32e2bcd65b4c6c76a4067
DANILO LISBOA DA SILVA BASTOS <danilo.bastos@aluno.senac.rio>
2025-09-01 20:31:35 -0300

    a4

---
+++ aula_4.py
@@ -0,0 +1,52 @@
+
+#estruturas de repetições 
+'''contador = 1
+while contador <= 5:
+    print (f'contador: {contador}')
+    contador += 1'''
+
+'''password = input('digite senha ')
+while password != '1234':
+    senha = input('digite uma senha': ).strip()
+    if password != '1234':
+        password = int(input('digite uma senha: '))
+
+print ('acesso liberado!')'''
+
+#VERIFICADOR DE MAIOR QUE DEZ
+'''for alfarroba in range (0, 10, 1):
+    print (alfarroba)'''
+
+'''while True:
+    try:
+        Numero = int(input('Digite um numero maior do que dez: '))
+        break
+    except:
+        print('Tente um valor válido')
+
+while Numero < 10:
+    print ('Esse numero é Menor que dez!')
+    Numero = int(input('Digite um número maior do que Dez: '))
+
+print('Esse número é maior que Dez!')'''
+
+#QUAL NUMERO VC QUER DA TBUADA?
+'''while True: 
+    try:
+        numero = int(input('De qual numero vc quer a tabuada: '))
+        break
+    except:
+        print('valor invalido!')
+
+        
+for i in range (1, 11, 1):
+    print (numero * i)'''
+
+
+
+#CONTADOR DE ZERO A VINTE
+'''
+n = 0
+while n < 22:
+        print(n)
+        n += 2'''
