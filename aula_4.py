c75d5ba43706ea3b3842ae9894fd26180ec58a62
DANILO LISBOA DA SILVA BASTOS <danilo.bastos@aluno.senac.rio>
2025-09-01 21:14:40 -0300

    aula_4

--- aula_4.py
+++ aula_4.py
@@ -1,4 +1,5 @@
 
+
 #estruturas de repetições 
 '''contador = 1
 while contador <= 5:
@@ -12,6 +13,7 @@ while password != '1234':
         password = int(input('digite uma senha: '))
 
 print ('acesso liberado!')'''
+
 
 #VERIFICADOR DE MAIOR QUE DEZ
 '''for alfarroba in range (0, 10, 1):
@@ -29,6 +31,7 @@ while Numero < 10:
     Numero = int(input('Digite um número maior do que Dez: '))
 
 print('Esse número é maior que Dez!')'''
+
 
 #QUAL NUMERO VC QUER DA TBUADA?
 '''while True: 
@@ -50,3 +53,56 @@ n = 0
 while n < 22:
         print(n)
         n += 2'''
+
+
+#LISTAS E TUPLAS
+'''
+nome = ['joão', 22, 4800, 'rua jota']
+print(type(nome))
+print(nome[0:4])
+
+telefone = 21
+nome.append(telefone)
+print (nome)
+
+for i in nome:
+    print (i)'''
+
+'''
+dados = [nome, idade, salario, local]
+nome = str(inbput('digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+salario = int(input('Digite seu salario: '))
+local = int(input('Digite seu DDD': ))
+for i in nome:
+    print(i)'''
+
+
+#LISTA DO CLIENTE
+cliente = []
+
+while True:
+    try:
+        num = int(input('Digite um numero: '))
+        cliente.append(num)
+        sair = input('o cliente deseja incluir mais um numero? (s/n)').upper().strip()
+        if sair == 'N':
+            break
+    except:
+        print('digite um número válido')
+
+for i in cliente:
+    print (i)
+
+
+#LISTA DA OPÇÃO
+'''    print ('1\n 2\n 3\n 4\n')
+while True:
+    try: 
+        opção = int(input('opção'))
+        if opção in [1,2,3,4]:
+            print('opção ok')
+        else:
+            print('opção invalida!')
+    except: 
+        print('Digite uym numero válido')'''
