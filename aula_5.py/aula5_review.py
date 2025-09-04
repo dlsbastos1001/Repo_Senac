3ef09623cc3c4254ce305403d2bebdc6b1407f19
DANILO LISBOA DA SILVA BASTOS <danilo.bastos@aluno.senac.rio>
2025-09-03 21:26:41 -0300

    a5

---
+++ aula_5.py/aula5_review.py
@@ -0,0 +1,1 @@
+#aula de revisão das aulas anteriores rfente ao conteúdo de python
---
+++ aula_5.py/basico.py
@@ -0,0 +1,55 @@
+#1
+'''nome = str(input('Qual o seu nome: '))
+idade = int(input('qual é a sua idade: '))
+cidade = str(input('qual é a sua cidade: '))
+print (f'Você é {nome}, tem {idade} anos e mora na cidade {cidade}')'''
+
+#2
+'''idade = int(input('qual é a sua idade: '))
+
+if idade > 18:
+    print('você é maior de idade!')
+else:
+    print ('Você é menor de idade!')'''
+
+#3
+'''for i in range(1, 11, 2):
+    print(i)'''
+
+#4
+'''contador = 0
+while contador <= 5:
+    print(contador)
+    contador += 1'''
+
+#5
+'''frutas = ['banana', 'pera', 'uva', 'laranja', 'limão']
+print (frutas)
+print (f'aprimeira fruta é {frutas[0]} e a ultima é {frutas[-1]}')'''
+
+#6
+'''frutas = ['banana', 'pera', 'uva', 'laranja', 'limão']
+frutas.insert(0, 'goiaba')
+frutas.remove('laranja')
+print (frutas)'''
+
+#7
+'''contador = 0
+nomes = ['ana', 'maria', 'pedro', 'joão', 'guilherme']
+for i in range (len(nomes)):
+    print (f'{nomes[contador]} você é @ {contador + 1}° da lista')
+    contador += 1'''
+
+#8
+teste = 1
+idades = [15, 19, 20, 21, 22]
+for i in range (len(idades)):
+    teste = (idades[i])
+    if teste > 18:
+        teste = ('maior de idade')
+    else:
+        teste = ('menor de idade')
+    print(f'{idades[i]} você é {teste}')
+
+#9
+
